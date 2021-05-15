7fb874b8464c1b89f284fb7b57d7829293a7c3f8
Bishko86 <polroldon@gmail.com>
2021-05-15 16:33:43 +0300

    added link for login or logout profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import Search from './SearchForm/Search';
+import SearchForm from './SearchForm/SearchForm';
 import style from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 const Header = (props) => {
-    return <header className={style.header}>
-        <h1 className={style.logo}>Note Face</h1>
-        <Search />
-        <div className={style.loginBlock}>
-            {props.isAuth ? <div>{props.login}-<button onClick={props.logout}>Logout</button></div> : <NavLink to={'/login'}>Login</NavLink>}
 
-        </div>
-    </header>;
+    return (
+        <header className={style.header}>
+            <h1 className={style.logo}>Note Face</h1>
+            <SearchForm />
+            <div className={style.loginBlock}>
+                {
+                    props.isAuth ?
+                        <div>
+                            <span style={{ color: 'white', marginRight: '15px' }}> {props.login} </span>
+                            <NavLink onClick={props.logout} to={'/login'}>Logout</NavLink>
+                        </div> :
+                        <div><NavLink to={'/login'}>Login</NavLink> </div>
+                }
+            </div>
+        </header>);
 }
+
 export default Header;
