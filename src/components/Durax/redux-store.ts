d515f52a0446e55b114504fa0b62cfb253901cbc
Bishko86 <polroldon@gmail.com>
2021-06-06 23:49:57 +0300

    migrated to the typescript

---
+++ src/components/Durax/redux-store.ts
@@ -0,0 +1,32 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import profileReducer from './profile-reducer'
+import dialogReducer from './dialog-reducer'
+import navbarReducer from './navbar-reducer'
+import usersReducer from './users-reducer';
+import authReducer from './auth-reducer';
+import appReducer from './app-reducer';
+
+import thunk from 'redux-thunk';
+
+let rootReducer = combineReducers({
+    profilePage: profileReducer,
+    dialogPage: dialogReducer,
+    navbarPage: navbarReducer,
+    usersPage: usersReducer,
+    auth: authReducer,
+    app: appReducer
+});
+
+type RootReducerType = typeof rootReducer
+
+export type AppStateType = ReturnType<RootReducerType>
+
+
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)
+));
+
+//@ts-ignore
+window.store = store;
+export default store;
