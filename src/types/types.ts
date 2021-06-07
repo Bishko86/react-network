d0ca2b9c2734652a623c6bc5d7037ce64cfba99b
Bishko86 <polroldon@gmail.com>
2021-06-07 21:44:30 +0300

    took out  profile-reducer types to separete file

--- src/types/types.ts
+++ src/types/types.ts
@@ -37,3 +37,58 @@ export type LogoutApiType = {
   messages: Array<string>,
   data: any
 }
+
+//profile reducer type
+
+export type ProfileType = {
+  userId: number
+  lookingForAJob: boolean
+  lookingForAJobDescription: string | null
+  fullName: string
+  contacts: ContactsType
+  photos: PhotoType
+
+}
+
+export type PhotoType = {
+  small: string | null
+  large: string | null
+}
+export type ContactsType = {
+  github: string | null
+  vk: string | null
+  facebook: string | null
+  instagram: string | null
+  twitter: string | null
+  website: string | null
+  youtube: string | null
+  mainLink: string | null
+}
+
+export type PostType = {
+  id: number
+  post: string
+  likes: number
+  liked: boolean
+  date: Date | string
+}
+export type InitialStateType = {
+  posts: Array<PostType>
+  profile: ProfileType | null
+  status: string
+  error: boolean | { error: {}; request: string | null; }
+}
+
+
+// function return types to literal types
+
+export function inferLiteralFromString<T extends string>(arg: T): T {
+  return arg;
+}
+export function inferLiteral<U, T extends U>(arg: T): T {
+  return arg;
+}
+export function inferStringLiteral<T extends string>(arg: T): T {
+  return inferLiteral<string, T>(arg);
+}
+
